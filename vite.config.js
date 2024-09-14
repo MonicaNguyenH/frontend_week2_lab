f89baa9505b10d6c92e1f42e32f462172abf11d4
MonicaNguyenH <144722804+MonicaNguyenH@users.noreply.github.com>
2024-09-14 11:56:29 -0700

    changes in viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<frontend_week2_lab>/'
+  base: '/frontend_week2_lab/'
 })
+
